4b2f25da08315a08deb008b687afc5808e4e78c4
Nirjal.shrestha14 <Nirjal.shrestha.14@gmail.com>
2026-02-07 13:20:30 +0545

    Interview model changes

--- backend/src/models/interview.model.js
+++ backend/src/models/interview.model.js
@@ -5,38 +5,6 @@ const interviewSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Application',
         required: true,
-    },
-    recruiter: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    student: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    interviewType: {
-        type: String,
-        enum: ['online', 'inoffice'],
-        required: true,
-    },
-    date: {
-        type: Date,
-        required: true,
-    },
-    time: { 
-        type: String,
-        required: true,
-    },
-    locationOrLink: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        enum: ['scheduled', 'completed', 'canceled'],
-        default: 'scheduled'
     }
 }, { timestamps: true });
 
