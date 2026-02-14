553a0adee039049882563d14f5ac2e566f1e9b75
Nirjal.shrestha14 <Nirjal.shrestha.14@gmail.com>
2026-02-14 13:06:05 +0545

    Chatbot changes

--- backend/src/models/chatbotSetting.model.js
+++ backend/src/models/chatbotSetting.model.js
@@ -1,20 +1,5 @@
 import mongoose from "mongoose";
 
-const chatbotSettingSchema = new mongoose.Schema({
-    singletonKey: {
-        type: String,
-        default: "kamchaiyo-main-settings",
-        unique: true,
-    },
-    systemPrompt: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    updatedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-}, { timestamps: true });
+
 
 export const ChatbotSetting = mongoose.model("ChatbotSetting", chatbotSettingSchema);
