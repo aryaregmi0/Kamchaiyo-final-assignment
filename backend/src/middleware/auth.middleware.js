b3f501b7b79cbb388f16c62bd78d34b2831cb762
Nirjal.shrestha14 <Nirjal.shrestha.14@gmail.com>
2026-02-21 12:51:42 +0545

    Changes in auth

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,6 +3,10 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
+
+});
+
+
 export const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
@@ -38,4 +42,3 @@ export const isRecruiter = asyncHandler(async(req, _, next) => {
         throw new ApiError(403, "Access denied. Only recruiters can perform this action.");
     }
     next();
-});
